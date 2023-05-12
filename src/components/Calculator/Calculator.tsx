a28195c4c6834ee508b0ab1ce6ec39457d907609
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:18:43 +0300

    fix: configTheme

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,35 +1,9 @@
-import {Button, Col, Input, Layout, Row, Typography} from 'antd';
-import {useState} from 'react';
-import {isEmpty} from 'lodash';
+import {Button, Col, Input, Layout, Row} from 'antd';
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const [currentNumber, setCurrentNumber] = useState<string>('');
   const mathActions: string[] = ['+', '-', '*', '/', '%'];
 
-  const [historyList, setHistoryList] = useState<string[]>([]);
-
-  const doAction = (action: string) => {
-    const lastIndex = historyList.length - 1;
-    if (!isEmpty(currentNumber)) {
-      setHistoryList([...historyList, currentNumber, action]);
-      setCurrentNumber('');
-    } else {
-      const res = [...historyList];
-      res[lastIndex] = action
-      setHistoryList(res);
-    }
-  }
-
-  const onChange = (newNumber: string | number) => {
-    setCurrentNumber(`${currentNumber}${newNumber}`);
-  }
-
-  const calc = () => {
-    const calcArr = [...historyList];
-    calcArr.pop();
-    return  eval(calcArr.join(' ')) || 0;
-  }
 
   return (
     <Layout style={{
@@ -37,27 +11,15 @@ export const Calculator = () => {
       margin: '0 auto',
     }}>
       <Layout.Content>
-        <Row gutter={[0, 8]}>
-          <Col span={24}>
-            <Typography>
-              <p>
-                {historyList.join(' ')}
-              </p>
-              <p>{calc()}</p>
-            </Typography>
-          </Col>
-          <Col span={24}>
-            <Input
-              size={'large'}
-              value={currentNumber}
-              onChange={(e) => onChange(e.currentTarget.value)}
-            />
-          </Col>
+        <Col span={24}>
+          <Input size={'large'} value={3} disabled />
+        </Col>
+        <Row>
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map(n => (
                 <Col span={7} key={n}>
-                  <Button onClick={() => onChange(n)} shape={'circle'} size={'large'}>{n}</Button>
+                  <Button shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
@@ -66,7 +28,7 @@ export const Calculator = () => {
             <Row gutter={[8, 8]}>
               {mathActions.map(n => (
                 <Col span={24} key={n}>
-                  <Button onClick={() => doAction(n)} shape={'circle'} size={'large'}>{n}</Button>
+                  <Button shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
