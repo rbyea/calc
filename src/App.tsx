2f673a799f004d527033a133be9ecde64d7e2028
taraswww777 <taraswww777@mail.ru>
2023-05-12 16:07:36 +0300

    fix: package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import {ConfigProvider} from 'antd';
 import {ANT_THEME_CONFIG} from 'src/constants/configTheme.ts';
+import {CommonLayout} from 'src/components/CommonLayout';
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <ConfigProvider theme={ANT_THEME_CONFIG}>
-
+      <CommonLayout />
     </ConfigProvider>
   )
 }
